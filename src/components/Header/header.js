5c54a72e074e96c1274ba2497c0bbad21b2e5788
Morgan Sommer <morgana.sommer@gmail.com>
2020-04-17 17:03:37 -0400

    header working

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './header.css';
 
 const Header = () => (
     <nav className=" navbar navbar-expand-lg navbar-light bg-light">
