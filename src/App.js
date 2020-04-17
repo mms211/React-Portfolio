814f199e651e05788eb636bb60e6e196170cff63
Morgan Sommer <morgana.sommer@gmail.com>
2020-04-17 17:49:11 -0400

    footer working

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Main from './components/Main/main';
 import Header from './components/Header/header';
+import Footer from './components/Footer/footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => (
+    <nav class="navbar fixed-bottom footer">
+        <p class="copyright"> Copyright © </p>
+    </nav>
+);
+
+export default Footer;
