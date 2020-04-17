faeefc1856210755e99f8e6b5a02799dd005d7a0
Morgan Sommer <morgana.sommer@gmail.com>
2020-04-17 18:42:38 -0400

    Contact Page added

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,12 +1,40 @@
 import React, { Component } from "react";
+import './contact.css';
 
 class Contact extends Component {
     render() {
-        return(
-            <div>
-                <h1>
-                    Contact
-                </h1>
+        return (
+            <div className="body">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="card box">
+                                <div className="card-body">
+                                    <div className="boxheader">
+                                        <h2> Contact Me</h2>
+                                    </div>
+                                    <form>
+                                        <div className="form-group">
+                                            <label htmlFor="exampleFormControlInput1">Name</label>
+                                            <input type="email" className="form-control" id="exampleFormControlInput1"
+                                                placeholder="John Smith" />
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="exampleFormControlInput1">Email</label>
+                                            <input type="email" className="form-control" id="exampleFormControlInput1"
+                                                placeholder="example@gmail.com" />
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="exampleFormControlTextarea1">Message</label>
+                                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                        </div>
+                                        <button type="button" className="btn btn-info">Submit</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
